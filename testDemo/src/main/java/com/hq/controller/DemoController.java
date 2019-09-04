a2693d24d3c0c3bbe62409a7b3308f8094921774
布莱恩·皮特 <@LAPTOP-IMJNAMFR>
2019-09-04 16:24:04 +0800

    安昊修改

--- testDemo/src/main/java/com/hq/controller/DemoController.java
+++ testDemo/src/main/java/com/hq/controller/DemoController.java
@@ -7,8 +7,11 @@ public class DemoController {
 		System.out.println("这是controller");
 	}
 	
+	
 	public void show2() {
 		System.out.println("demo测试操作");
 	}
-	
+	public void show3() {
+		System.out.println("demo2测试操作");
+	}
 }
